8beb62daf97ef2b07081edd669f16d1f4b84308c
Sushant <mitthujangra4229@gmail.com>
2022-03-05 23:28:38 +0530

    Added fn headers

--- math.py
+++ math.py
@@ -1,11 +1,15 @@
+# Add Implementation
 def add(x,y):
     pass
 
+# Subtract Implementation
 def subtrack(x,y):
     pass
 
+# Multiply Implementation
 def multiply(x,y):
     pass
 
+# Divide Implementation
 def divide(x,y):
     pass
